cd23300d5655a4a6e565b531fa159d5b1d45d269
Byung Kyu Kim <cdeclare@gmail.com>
2021-03-19 17:34:24 +0900

    feat: add async interface

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,45 +10,63 @@ using System.Net;
 
 namespace mvcapp.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        private static int _callcount = 0;
+		private static int _callcount = 0;
 
-        // [Route("/")]
-        [Route("/{sleep?}")]
-        public IActionResult Index(int sleep)
-        {
-            if (sleep > 0) {
-                Task.Delay(sleep).Wait();
-            }
-            ++_callcount;
+		private void ValueSet(int sleep)
+		{
+			ViewData["Version"] = "0.6 / net5.0";
+			ViewData["Hostname"] = Dns.GetHostName();
+			ViewData["CallCount"] = _callcount;
+			ViewData["Sleep"] = sleep;
+			ViewData["RemoteAddr"] = Request.HttpContext.Connection.RemoteIpAddress;
+			ViewData["XForwardedFor"] = Request.Headers["X-Forwarded-For"].FirstOrDefault();
+			ViewData["UserAgent"] = Request.Headers["User-Agent"].ToString();
+		}
 
-            ViewData["Hostname"] = Dns.GetHostName();
-            ViewData["CallCount"] = _callcount;
-            ViewData["Sleep"] = sleep;
-            ViewData["RemoteAddr"] = Request.HttpContext.Connection.RemoteIpAddress;
-            ViewData["XForwardedFor"] = Request.Headers["X-Forwarded-For"].FirstOrDefault();
-            ViewData["UserAgent"] = Request.Headers["User-Agent"].ToString();
+		[Route("/")]
+		[Route("/sync/{sleep?}")]
+		public IActionResult Index(int sleep)
+		{
+			if (sleep > 0) {
+				Task.Delay(sleep).Wait();
+			}
 
-            return View();
-        }
+			++_callcount;
+			ValueSet(sleep);
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[Route("/async/{sleep?}")]
+		public async Task<IActionResult> IndexAs(int sleep)
+		{
+			if (sleep > 0) {
+				await Task.Delay(sleep);
+			}
+			++_callcount;
+			ValueSet(sleep);
+
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
